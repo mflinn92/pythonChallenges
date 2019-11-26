0ee7428198088a271e44788ed7936d26f11a0ac4
Michael Flinn <mflinn92@gmail.com>
2019-11-26 10:53:35 -0500

    Calculates the maximum height that each building in grid can be increased given the constraint that the contour of the skyline must be preserved from every viewing angle.

---
+++ max_increasing_skyline.py
@@ -0,0 +1,29 @@
+import unittest
+
+def maxIncreaseKeepingSkyline(grid) -> int:
+    # get max height allowed for each row in the grid
+    max_row_heights = []
+    for row in grid:
+        max_row_heights.append(max(row))
+    
+    #get max height allowed for each column in the grid
+    max_col_heights = []
+    for i in range(len(grid[0])):
+        col = []
+        for j in range(len(grid)):
+            col.append(grid[j][i])
+        max_col_heights.append(max(col))
+    total_increase = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            # total allowed height increase is the minimum of the max increase for a given row and column
+            total_increase += min(max_row_heights[i], max_col_heights[j]) - grid[i][j]
+    return total_increase
+
+class SkylineTest(unittest.TestCase):
+    def test(self):
+        test_grid = [[3,0,8,4],[2,4,5,7],[9,2,6,3],[0,3,1,0]]
+        self.assertEqual(maxIncreaseKeepingSkyline(test_grid), 35)
+
+if __name__ == '__main__':
+    unittest.main()
