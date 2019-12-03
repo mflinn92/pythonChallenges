0f6f60941322127dc34c287db63830066350ad24
Michael Flinn <mflinn92@gmail.com>
2019-12-03 14:38:24 -0500

    Implemented more efficient O(n) bucket sort solution in addition to initial solution

--- k_most_frequent.py
+++ k_most_frequent.py
@@ -1,7 +1,7 @@
 import unittest
 #Given a non-empty array of integers, return the k most frequent elements.
 
-def top_k_frequent(nums, k):
+def top_k_frequent_initial(nums, k):
     """
     :type nums: List[int]
     :type k: int
@@ -20,14 +20,39 @@ def top_k_frequent(nums, k):
         n -= 1
     return output
 
+def top_k_frequent_bucket_sort(nums ,k):
+    frequencies = {}
+    for num in nums:
+        frequencies[num] = frequencies.get(num, 0) + 1
+    freq_buckets = [None] * (len(nums) + 1)
+
+    for num, freq in frequencies.items():
+        if not freq_buckets[freq]:
+            freq_buckets[freq] = [num]
+        else:
+            freq_buckets[freq].append(num)
+    result = []
+    for val in reversed(freq_buckets):
+        if val:
+            result += val
+        if len(result) == k:
+            break
+    return result
+
+
+
 class Test(unittest.TestCase):
     def test_most_frequent(self):
         test_list = [1,1,1,2,2,3]
-        self.assertEqual(top_k_frequent(test_list, 2), [1,2])
+        self.assertEqual(top_k_frequent_initial(test_list, 2), [1,2])
 
     def test_invalid_k(self):
         test_list = [2,3,4,2]
-        self.assertEqual(top_k_frequent(test_list, 7), -1)
+        self.assertEqual(top_k_frequent_initial(test_list, 7), -1)
+    
+    def test_bucket_method(self):
+        test_list = [1,1,1,2,2,3]
+        self.assertEqual(top_k_frequent_bucket_sort(test_list, 2), [1,2])
 
 if __name__ == '__main__':
     unittest.main()
